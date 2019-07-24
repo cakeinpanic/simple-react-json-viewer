cc4e406183c93e1b6410b73075bd64114a4b6682
cakeinpanic <cakeinpanic@gmail.com>
2019-07-24 12:02:04 +0300

    update build script

---
+++ precache-manifest.b083edef1b809e107396225344fd7b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de272fd01186b6c4484a94821626596",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c4676f24cc2a2ebc3b",
+    "url": "/static/css/main.029d8bd5.chunk.css"
+  },
+  {
+    "revision": "67e11cb4594429fe8df4",
+    "url": "/static/js/2.2676d12f.chunk.js"
+  },
+  {
+    "revision": "f1c4676f24cc2a2ebc3b",
+    "url": "/static/js/main.1210ffa6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
